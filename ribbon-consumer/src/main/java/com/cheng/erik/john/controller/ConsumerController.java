69ef9e9be3c3c5c9351cb43399c9f93d7ab9cb31
JohnErikCheng <dong19910925@126.com>
2019-05-29 17:06:33 +0800

    hystrix

--- ribbon-consumer/src/main/java/com/cheng/erik/john/controller/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/cheng/erik/john/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.cheng.erik.john.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,14 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "defaultStores")
     @GetMapping("/hello")
     public String hello() {
         String body = restTemplate.getForEntity("http://ribbon-provider/ribbon", String.class).getBody();
-        System.out.println("body---->" + body);
         return body;
+    }
+
+    public String defaultStores() {
+        return "Ribbon + hystrix ,提供者服务挂了";
     }
 }
